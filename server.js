d6f7fc20d0d780fea763a64b5d09ce409820cc0c
CGalko <cash.galko12@ncf.edu>
2021-04-20 13:00:37 -0400

    Added the starter server.js. Added all of the needed node moduels & package.json.

---
+++ server.js
@@ -0,0 +1,82 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Current reservations & waitlist (DATA)
+
+const reservations = [
+  {
+    routeName: 'yoda',
+    name: 'Yoda',
+    phoneNum: 'Jedi Master',
+    email: 900,
+    id: 2000,
+  },
+ 
+];
+
+const waitList = [
+
+];
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+// Displays all current table reservations
+app.get('/api/reservations', (req, res) => res.json(reservations));
+// Displays everyone on the waitlist
+app.get('/api/waitlist', (req, res) => res.json(waitList));
+
+// Displays a single character, or returns false
+app.get('/api/characters/:character', (req, res) => {
+  const chosen = req.params.character;
+
+  console.log(chosen);
+
+  /* Check each character routeName and see if the same as "chosen"
+   If the statement is true, send the character back as JSON,
+   otherwise tell the user no character was found */
+
+  for (let i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post('/api/characters', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newCharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+  res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
