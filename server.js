0a7d564629db596db4047bc9a6adbce6282bf630
CGalko <cash.galko12@ncf.edu>
2021-04-20 13:56:02 -0400

    Added logic for reservations API to check length and push in correct array

--- server.js
+++ server.js
@@ -43,39 +43,31 @@ app.get('/api/reservations', (req, res) => res.json(reservations));
 // Displays everyone on the waitlist
 app.get('/api/waitlist', (req, res) => res.json(waitList));
 
-// Displays a single character, or returns false
-// app.get('/api/characters/:character', (req, res) => {
-//   const chosen = req.params.character;
+// Checks
+app.get('/api/reservations', (req, res) => {
+  
+    if (reservations.length <= 5) {
+      return res.json(false);
+    } else {
+    return res.json(true);
+    }
 
-//   console.log(chosen);
+});
 
-//   /* Check each character routeName and see if the same as "chosen"
-//    If the statement is true, send the character back as JSON,
-//    otherwise tell the user no character was found */
+// Create New Characters - takes in JSON input
+app.post('/api/characters', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newCharacter = req.body;
 
-//   for (let i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newCharacter);
 
-//   return res.json(false);
-// });
-
-// // Create New Characters - takes in JSON input
-// app.post('/api/characters', (req, res) => {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   const newCharacter = req.body;
-
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-//   console.log(newCharacter);
-
-//   characters.push(newCharacter);
-//   res.json(newCharacter);
-// });
+  characters.push(newCharacter);
+  res.json(newCharacter);
+});
 
 // Starts the server to begin listening
 
